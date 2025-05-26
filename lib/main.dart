c2273ab0f41df5c4a2277a56d3bf2ebfc1708227
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-05-26 17:13:13 -0300

    Decided to use sqflite for data and tests with theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+  var unhas = ["0.0","0.0","0.0","0.0","0.0"];
+ 
 
 void main() {
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Nail Measurer',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 183, 0, 255)),
+        scaffoldBackgroundColor: Theme.of(context).colorScheme.primary
       ),
       home: const MyHomePage(title: 'Medidor de Unhas'),
     );
@@ -29,19 +35,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var unhas = [0.0,0.0,0.0,0.0,0.0];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
+      appBar: AppBar(title: Text(widget.title),backgroundColor: Theme.of(context).colorScheme.primary),
       body: Center(
         child: Stack(
           children: <Widget>[
             Container(
               alignment: Alignment.bottomCenter,
               decoration: BoxDecoration(
-                color: Color.fromRGBO(100, 100, 100, 100),
+                color: Theme.of(context).colorScheme.primary
               ),
               height: 560,
               width: 600,
@@ -51,23 +56,56 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
 
-            Positioned(
-              top: 175,
-              left: 140,
+            InputNail(positionTop: 175, positionLeft: 140),
+            InputNail(positionTop: 130, positionLeft: 220),
+            InputNail(positionTop: 110, positionLeft: 295),
+            InputNail(positionTop: 135, positionLeft: 355),
+            InputNail(positionTop: 280, positionLeft: 400),
+
+            saveNail()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InputNail extends StatelessWidget {
+  final double positionTop;
+  final double positionLeft;
+
+
+   const InputNail({
+    Key? key,
+    required this.positionTop,
+    required this.positionLeft,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+              top: positionTop,
+              left: positionLeft,
               child: SizedBox(
                 width: 60,
                 height: 40,
                 child: TextField(
                   onChanged: (text) {
-                    unhas[0] = double.parse(text);
+                    unhas[0] = text;
                     print(unhas[0]);
                   },
                   decoration: InputDecoration(hintText: '2.5cm'),
                 ),
               ),
-            ),
+            );
+  }
 
-            TextButton(
+}
+
+class saveNail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
               style: ButtonStyle(
                 foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
               ),
@@ -75,10 +113,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
               },
               child: Text('Salvar unhas'),
-            ),
-          ],
-        ),
-      ),
-    );
+            );
+    
   }
+
+void registerNail() async{
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setStringList('tamanhoDasUnhas', unhas);
+}
+
+}
+
+class NailRegister {
+  final List<double> tamanhos;
+  final DateTime data;
+  final bool bonsHabitos;
+  final int id;
+
+  const NailRegister({required this.tamanhos, required this.data, required this.bonsHabitos, required this.id});
 }
