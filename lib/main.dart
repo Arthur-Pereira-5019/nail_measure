95bc48e5dd39d98cd093e1f10b149fd5ce726e24
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-06-02 18:44:35 -0300

    Backup of DB strcture and changes in homepage appearence

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-  var unhas = ["0.0","0.0","0.0","0.0","0.0"];
- 
+var unhas = ["0.0", "0.0", "0.0", "0.0", "0.0"];
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -17,8 +16,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Nail Measurer',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 183, 0, 255)),
-        scaffoldBackgroundColor: Theme.of(context).colorScheme.primary
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 183, 0, 255),
+        ),
+        scaffoldBackgroundColor: Theme.of(context).colorScheme.primary,
       ),
       home: const MyHomePage(title: 'Medidor de Unhas'),
     );
@@ -35,35 +36,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title),backgroundColor: Theme.of(context).colorScheme.primary),
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
       body: Center(
-        child: Stack(
-          children: <Widget>[
-            Container(
-              alignment: Alignment.bottomCenter,
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary
-              ),
-              height: 560,
-              width: 600,
-              child: Image(
-                alignment: Alignment.bottomCenter,
-                image: AssetImage((('resources/left_hand.png'))),
-              ),
-            ),
-
-            InputNail(positionTop: 175, positionLeft: 140),
-            InputNail(positionTop: 130, positionLeft: 220),
-            InputNail(positionTop: 110, positionLeft: 295),
-            InputNail(positionTop: 135, positionLeft: 355),
-            InputNail(positionTop: 280, positionLeft: 400),
-
-            saveNail()
-          ],
+        child: Scrollbar(
+          child: ListView(children: [DisplayUnha(), DisplayUnha(), SaveNail()]),
         ),
       ),
     );
@@ -74,8 +56,7 @@ class InputNail extends StatelessWidget {
   final double positionTop;
   final double positionLeft;
 
-
-   const InputNail({
+  const InputNail({
     Key? key,
     required this.positionTop,
     required this.positionLeft,
@@ -84,51 +65,195 @@ class InputNail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Positioned(
-              top: positionTop,
-              left: positionLeft,
-              child: SizedBox(
-                width: 60,
-                height: 40,
-                child: TextField(
-                  onChanged: (text) {
-                    unhas[0] = text;
-                    print(unhas[0]);
-                  },
-                  decoration: InputDecoration(hintText: '2.5cm'),
-                ),
-              ),
-            );
+      top: positionTop,
+      left: positionLeft,
+      child: SizedBox(
+        width: 60,
+        height: 40,
+        child: TextField(
+          onChanged: (text) {
+            unhas[0] = text;
+            print(unhas[0]);
+          },
+          decoration: InputDecoration(hintText: '2.5cm'),
+        ),
+      ),
+    );
   }
-
 }
 
-class saveNail extends StatelessWidget {
+class SaveNail extends StatelessWidget {
+  const SaveNail({super.key});
+
   @override
   Widget build(BuildContext context) {
     return TextButton(
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-              ),
-              onPressed: () {
-
-              },
-              child: Text('Salvar unhas'),
-            );
-    
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Tela2()),
+        );
+      },
+      child: Text('Salvar unhas'),
+    );
   }
+}
 
-void registerNail() async{
+void registerNail() async {
   final prefs = await SharedPreferences.getInstance();
   await prefs.setStringList('tamanhoDasUnhas', unhas);
 }
 
+class Tela2 extends StatelessWidget {
+  const Tela2({
+    super.key,
+    //required this.positionTop,
+    //required this.positionLeft,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dados das Unhas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 131, 32, 79),
+        ),
+        scaffoldBackgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      home: const NailTablePage(title: 'Medidor de Unhas'),
+    );
+  }
 }
 
-class NailRegister {
-  final List<double> tamanhos;
-  final DateTime data;
-  final bool bonsHabitos;
-  final int id;
+class NailTablePage extends StatefulWidget {
+  const NailTablePage({super.key, required this.title});
 
-  const NailRegister({required this.tamanhos, required this.data, required this.bonsHabitos, required this.id});
+  final String title;
+
+  @override
+  State<NailTablePage> createState() => _NailTablePage();
+}
+
+class _NailTablePage extends State<NailTablePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              height: 560,
+              width: 600,
+              child: Opacity(
+                opacity: 0.3,
+                child: Image(
+                  alignment: Alignment.bottomCenter,
+                  image: AssetImage((('resources/left_hand.png'))),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NailTable extends StatelessWidget {
+  //final double positionTop;
+  //final double positionLeft;
+
+  const NailTable({
+    super.key,
+    //required this.positionTop,
+    //required this.positionLeft,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Expanded(
+            child: Text('Name', style: TextStyle(fontStyle: FontStyle.italic)),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text('Age', style: TextStyle(fontStyle: FontStyle.italic)),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text('Role', style: TextStyle(fontStyle: FontStyle.italic)),
+          ),
+        ),
+      ],
+      rows: const <DataRow>[
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Sarah')),
+            DataCell(Text('19')),
+            DataCell(Text('Student')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Janine')),
+            DataCell(Text('43')),
+            DataCell(Text('Professor')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('William')),
+            DataCell(Text('27')),
+            DataCell(Text('Associate Professor')),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class DisplayUnha extends StatelessWidget {
+  const DisplayUnha({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          alignment: Alignment.bottomCenter,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          height: 560,
+          width: 600,
+          child: Image(
+            alignment: Alignment.bottomCenter,
+            image: AssetImage((('resources/left_hand.png'))),
+          ),
+        ),
+
+        InputNail(positionTop: 175, positionLeft: 140),
+        InputNail(positionTop: 130, positionLeft: 220),
+        InputNail(positionTop: 110, positionLeft: 295),
+        InputNail(positionTop: 135, positionLeft: 355),
+        InputNail(positionTop: 280, positionLeft: 400),
+      ],
+    );
+  }
 }
---
+++ lib/nailRegister.dart
@@ -0,0 +1,116 @@
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+
+class NailDatabase {
+  late Database database;
+
+Future<void> createDatabase() async {
+    database = await openDatabase(
+
+    join(await getDatabasesPath(), 'naildatabase.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE nails(id INTEGER PRIMARY KEY, data TEXT, tMiEs REAL, tAnEs REAL, tMeEs REAL, tInEs REAL, tPoEs REAL, tPoDi REAL, tInDi REAL, tMeDi REAL, tAnDi REAL, tMiDi REAL)',
+      );
+    },
+    version: 1,
+  );
+}
+
+  Future<void> registerNail(Nail nail) async {
+    final db = database;
+
+    await db.insert(
+      'nails',
+      nail.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Nail>> nails() async {
+  final db = database;
+  final List<Map<String, Object?>> nailMaps = await db.query('nails');
+
+  return nailMaps.map((map) => Nail.fromMap(map)).toList();
+}
+
+  Future<void> updateNail(Nail nail) async {
+    final db = database;
+
+    await db.update(
+      'nails',
+      nail.toMap(),
+      where: 'id = ?',
+      whereArgs: [nail.id],
+    );
+  }
+
+  Future<void> deleteNail(int id) async {
+    final db = database;
+
+    await db.delete(
+      'nails',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
+
+class Nail {
+  final int id;
+  final DateTime data;
+  final double tMiEs;
+  final double tAnEs;
+  final double tMeEs;
+  final double tInEs;
+  final double tPoEs;
+  final double tPoDi;
+  final double tInDi;
+  final double tMeDi;
+  final double tAnDi;
+  final double tMiDi;
+
+  Nail({required this.id, required this.data, required this.tMiEs, required this.tAnEs, required this.tMeEs, required this.tInEs, required this.tPoEs, required this.tPoDi, required this.tInDi, required this.tMeDi, required this.tAnDi, required this.tMiDi});
+
+    Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'date': data.toIso8601String(),
+      'tMiEs': tMiEs,
+      'tAnEs': tAnEs,
+      'tMeEs': tMeEs,
+      'tInEs': tInEs,
+      'tPoEs': tPoEs,
+      'tPoDi': tPoDi,
+      'tInDi': tInDi,
+      'tMeDi': tMeDi,
+      'tAnDi': tAnDi,
+      'tMiDi': tMiDi,
+    };
+  }
+
+  static Nail fromMap(Map<String, dynamic> map) {
+    return Nail(
+      id: map['id'],
+      data: DateTime.parse(map['data']),
+      tMiEs: map['tMiEs'],
+      tAnEs: map['tAnEs'],
+      tMeEs: map['tMeEs'],
+      tInEs: map['tInEs'],
+      tPoEs: map['tPoEs'],
+      tPoDi: map['tPoDi'],
+      tInDi: map['tInDi'],
+      tMeDi: map['tMeDi'],
+      tAnDi: map['tAnDi'],
+      tMiDi: map['tMiDi'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Nail{id: $id, Mindinho Esquerdo: $tMiEs, Anelar Esquerdo: $tAnEs}';
+  }
+}
